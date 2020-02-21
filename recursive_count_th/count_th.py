9883108fdbbbbc91c6720db93fe56da6a62b2c3c
Petar Scepanovic <hiborija@gmail.com>
2020-02-21 10:54:42 +0100

    finished recursive count, tests are passing

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,9 +1,8 @@
 """
-Your function should take in a signle parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should take in a single parameter (a string `word`)
+Your function should return a count of how many occurrences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 """
-
 """
 Polya
 
@@ -12,11 +11,11 @@ What is out input?
 The input is string
 
 What are the restrictions?
-The restriction is that we need to find the occurences of "th" word within a string we are passing
+The restriction is that we need to find the occurrences of "th" word within a string we are passing
 The function must be recursive, no loops are allowed
 
 What does our function return?
-function should return a count of how many occurences of "th" occur within word. Case matters
+function should return a count of how many occurrences of "th" occur within word. Case matters
 
 2) Plan
 
@@ -24,8 +23,13 @@ function should return a count of how many occurences of "th" occur within word.
 
 
 def count_th(word, target="th"):
-    pass
+    n1 = len(word)
+    n2 = len(target)
 
+    if n1 == 0 or n1 < n2:
+        return 0
 
+    if word[0:n2] == target:
+        return count_th(word[n2:], target) + 1
 
-
+    return count_th(word[n2 - 1:], target)
